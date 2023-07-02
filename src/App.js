660f77b193fc95c8f9f4175a9a587e7369fe716c
iluengas <isaacluengas123@gmail.com>
2023-07-01 18:20:26 -0700

    Commit message

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import './App.css';
+import { useState } from 'react';
+
+function Square({ value, onSquareClick }) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return
+    }
+
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    }
+    else {
+      nextSquares[i] = "0";
+    }
+
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext)
+  }
+
+  return (
+    <div className='parent'>
+      <div className='dash-board'>
+        <div className='text'>
+          Tic-Tac
+        </div>
+        <div className='text2'>
+          Toe
+        </div>
+        
+      </div>
+      <div className='header'>
+        <div className='text3'>
+          Enjoy the tic tac toe game !
+        </div>
+      </div>
+      <div className='game-board-container'>
+        <div className="game-board">
+          <div>
+            <div className="board-row">
+              <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+              <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+              <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+            </div>
+            <div className="board-row">
+              <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+              <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+              <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+            </div>
+            <div className="board-row">
+              <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+              <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+              <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+            </div>
+          </div>
+          <div className="status-container">
+            <div className="status">{status}</div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
